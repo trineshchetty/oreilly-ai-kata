58206b00a2856cf4491fcd4f448c11c901990a27
trineshchetty <trineshchetty@gmail.com>
2024-11-20 15:37:56 +0200

    Setup streamlit app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is the Oreilly AI Kata Project By Old Mutual Platform Engineering team.
+
+
+ADR - Architecture Decision Records
+Repo
+
+https://github.com/joelparkerhenderson/architecture-decision-record/tree/main/locales/en/templates/decision-record-template-by-michael-nygard
---
+++ src/schema/models.sql
@@ -0,0 +1,13 @@
+-- Product ID,ProductName, Merchant ID, Cluster ID, Cluster Label, Category ID,Category,Price,StockQuantity,Description,Rating
+CREATE TABLE Products (
+)
+-- Merchant ID,MerchantName,MerchantLocation,MerchantRating
+CREATE TABLE Orders (
+)
+
+
+-- Ensure you have the Database created in RDS, and is publicly accessible.
+-- install pgadmin (Postgres DB Client) and create a new server connection. Specify db endpoint, port, username, password.
+-- Run the above SQL statements in PGADMIN SQL Editor, to create the tables.
+-- Once the tables are created, import the data from the CSV files into the tables.
+-- Test the queries in the SQL Editor, to ensure the data is imported correctly.
